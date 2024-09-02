b2a497a97c9bd3e608879ca465292bbc5dc02fc4
Erich Essmann <essmanne@gmail.com>
2024-09-02 14:03:42 +0100

    Fixed Memory leak

--- parallel/parallel_src/lib/communication/mpi_tools.h
+++ parallel/parallel_src/lib/communication/mpi_tools.h
@@ -9,6 +9,11 @@
 #ifndef MPI_TOOLS_HMESDXF2
 #define MPI_TOOLS_HMESDXF2
 
+#include <algorithm>
+#include <functional>
+#include <ranges>
+#include <numeric>
+
 #include "data_structure/parallel_graph_access.h"
 #include "partition_config.h"
 
@@ -83,45 +88,45 @@ namespace mpi_collective_tools {
     // Packs message buffer so that MPI_AlltoAllv can be used
     template<Container Input>
         requires Container<typename Input::value_type>
-    auto pack_messages(
-        Input const &messages) -> mpi_packed_message<typename Input::value_type::value_type> {
+    auto pack_messages(Input const &messages)
+        -> mpi_packed_message<typename Input::value_type::value_type> {
         using inner = typename Input::value_type;
         using element_type = typename inner::value_type;
 
-        std::vector<element_type> flattened_vector{};
-        std::vector<int> offsets{};
-        std::vector<int> lengths{};
-
-        int current_offset = 0;
-
-        for (auto const &subvector: messages) {
-            offsets.push_back(current_offset);
-            lengths.push_back(static_cast<int>(subvector.size()));
-
-            flattened_vector.insert(flattened_vector.end(), subvector.begin(), subvector.end());
-
-            current_offset += static_cast<int>(subvector.size());
+        // Flattening the vector of vectors using join_view
+        auto const flattened_view = messages | std::ranges::views::join;
+        std::vector<element_type> flattened_vector;
+        // converting view into vector
+        for (auto &&elem: flattened_view) {
+            flattened_vector.push_back(static_cast<decltype(elem) &&>(elem));
         }
+
+        // Calculating lengths using transform view
+        std::vector<int> lengths;
+        lengths.reserve(messages.size());
+        std::ranges::transform(messages, std::back_inserter(lengths),
+                               [](auto const &elem) { return static_cast<int>(elem.size()); });
+
+        // Calculating offsets using exclusive_scan
+        std::vector<int> offsets(messages.size());
+        std::exclusive_scan(lengths.begin(), lengths.end(), offsets.begin(), 0);
 
         return mpi_packed_message{flattened_vector, offsets, lengths};
     }
 
-    // Unpacks received message buffer received from alltoall
     template<typename Elem>
-    auto unpack_messages(mpi_packed_message<Elem> const &packed_message) -> std::vector<std::vector<Elem> > {
+    auto unpack_messages(mpi_packed_message<Elem> const &packed_message)
+        -> std::vector<std::vector<Elem> > {
         auto const &[recv_buf, recv_displs, recv_counts] = packed_message;
+        auto const recv_span = std::span(recv_buf);
         int num_ranks = static_cast<int>(recv_counts.size());
-        std::vector<std::vector<NodeID> > result(num_ranks);
+
+        std::vector<std::vector<Elem> > result;
+        result.reserve(num_ranks);
 
         for (int i = 0; i < num_ranks; ++i) {
-            int const count = recv_counts.at(i);
-            int const offset = recv_displs.at(i);
-
-            // Extract the subvector corresponding to rank `i`
-            std::vector subvector(recv_buf.begin() + offset, recv_buf.begin() + offset + count);
-
-            // Store it in the result vector at index `i`
-            result[i] = std::move(subvector);
+            auto const subspan = recv_span.subspan(recv_displs.at(i), recv_counts.at(i));
+            result.emplace_back(subspan.begin(), subspan.end());
         }
 
         return result;
@@ -140,20 +145,25 @@ namespace mpi_collective_tools {
         // Packing messages into vector and computing offsets and lengths for the sub messages
         auto [send_packed_messages, send_offsets, send_lengths] = mpi_collective_tools::pack_messages(sends);
 
+        if (send_offsets.size() != send_lengths.size()) {
+            throw(std::runtime_error("MPI_collective_tools::pack_messages()"));
+        } else if ((send_offsets.size() != size) || (send_lengths.size() != size)) {
+            throw(std::runtime_error("MPI_collective_tools::pack_messages()"));
+        }
+
         // Preparing receive buffers for the node ids, offsets, and lengths
         int total_message_size = 0;
         const int local_message_size = static_cast<int>(send_packed_messages.size());
         MPI_Allreduce(&local_message_size, &total_message_size, 1, MPI_INT, MPI_SUM, communicator);
 
-        auto recv_packed_messages = std::vector<element_type>(total_message_size);
-        auto recv_offsets = std::vector<int>(size);
-        auto recv_lengths = std::vector<int>(size);
+        auto recv_packed_messages = std::vector<element_type>(total_message_size, 0);
+        auto recv_offsets = std::vector<int>(size, 0);
+        auto recv_lengths = std::vector<int>(size, 0);
 
         auto mpi_error = MPI_Alltoallv(send_packed_messages.data(), send_lengths.data(), send_offsets.data(),
-                                           MPI_UNSIGNED_LONG_LONG, recv_packed_messages.data(), recv_lengths.data(),
-                                           recv_offsets.data(), MPI_UNSIGNED_LONG_LONG,
-                                           communicator);
-
+                                       MPI_UNSIGNED_LONG_LONG, recv_packed_messages.data(), recv_lengths.data(),
+                                       recv_offsets.data(), MPI_UNSIGNED_LONG_LONG,
+                                       communicator);
         return mpi_collective_tools::unpack_messages<element_type>({recv_packed_messages, recv_offsets, recv_lengths});
     }
 }
--- parallel/parallel_src/tests/parallel_contraction/parallel_contraction_mpi_test.cpp
+++ parallel/parallel_src/tests/parallel_contraction/parallel_contraction_mpi_test.cpp
@@ -6,12 +6,20 @@
 #include <vector>
 #include <mpi.h>
 #include <communication/mpi_tools.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 
 
-TEST_CASE("all to all vector of vectos", "[unit][mpi]") {
+TEST_CASE("all to all vector of vectors", "[unit][mpi]") {
     SECTION("emtpy case") {
-        const std::vector<std::vector<NodeID>> v_empty{{1}};
+        PEID rank, size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        const std::vector<std::vector<NodeID>> v_empty{{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
         auto vec = mpi_collective_tools::all_to_all(v_empty, MPI_COMM_WORLD);
-        REQUIRE(vec.empty());
+        MPI_Barrier(MPI_COMM_WORLD);
+        fmt::println("rank: {} -> {}", rank, vec);
+        REQUIRE(v_empty == vec);
     }
 }
